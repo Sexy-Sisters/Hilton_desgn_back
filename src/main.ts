0166b484a7ebf38a4be4f429814ad26f6e16e80f
DONGHAEY <dongheaey@gmail.com>
2024-02-10 08:53:48 +0000

    feat: add global prefix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({});
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Apis')
     .setDescription('Apis')
